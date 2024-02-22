81413f9d04752826a650b29b1b8c0ff038a6496a
LaKeRoK <LaKeRoK201@yandex.com>
2024-02-22 12:58:12 +0300

    complite task 2

--- main2.py
+++ main2.py
@@ -1,8 +1,9 @@
 import random
 import numpy as np
+import pandas as pd
 import time
+import matplotlib.pyplot as plt
 
-# Создание списков и массивов
 list1 = [random.random() for _ in range(1000000)]
 list2 = [random.random() for _ in range(1000000)]
 array1 = np.array(list1)
@@ -18,3 +19,10 @@ start_time = time.perf_counter()
 result_array = np.multiply(array1, array2)
 end_time = time.perf_counter()
 print("Время выполнения перемножения массивов NumPy:", end_time - start_time)
+
+
+
+data = pd.read_csv("data2.csv")
+plt.hist(data["ph"], bins=50, label="histogram for task 2")
+plt.show()
+
