a9f0ef69433ec21a32e1225b7d7fca06334e43f4
LaKeRoK <LaKeRoK201@yandex.com>
2024-02-22 13:23:54 +0300

    complite all tasks

--- main2.py
+++ main2.py
@@ -3,29 +3,32 @@ import numpy as np
 import pandas as pd
 import time
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-list1 = [random.random() for _ in range(1000000)]
-list2 = [random.random() for _ in range(1000000)]
-array1 = np.array(list1)
-array2 = np.array(list2)
+def show_time():
+
+    list1 = [random.random() for _ in range(1000000)]
+    list2 = [random.random() for _ in range(1000000)]
+    array1 = np.array(list1)
+    array2 = np.array(list2)
 
 
-start_time = time.perf_counter()
-result_list = [list1[i] * list2[i] for i in range(len(list1))]
-end_time = time.perf_counter()
-print("Время выполнения перемножения списков:", end_time - start_time)
+    start_time = time.perf_counter()
+    result_list = [list1[i] * list2[i] for i in range(len(list1))]
+    end_time = time.perf_counter()
+    print("Время выполнения перемножения списков:", end_time - start_time)
 
-start_time = time.perf_counter()
-result_array = np.multiply(array1, array2)
-end_time = time.perf_counter()
-print("Время выполнения перемножения массивов NumPy:", end_time - start_time)
+    start_time = time.perf_counter()
+    result_array = np.multiply(array1, array2)
+    end_time = time.perf_counter()
+    print("Время выполнения перемножения массивов NumPy:", end_time - start_time)
 
 ##########################################
 
-data = pd.read_csv("data2.csv")
-plt.hist(data["ph"], bins=50, label="histogram for task 2", )
-plt.show()
+def histograms():
+
+    data = pd.read_csv("data2.csv")
+    plt.hist(data["ph"], bins=50, label="histogram for task 2", )
+    plt.show()
 
 ################################
 
@@ -44,4 +47,8 @@ def plot3d():
     ax.set_title('3D Plot')
     plt.show()
 
-plot3d()
+
+if __name__ == "__main__":
+    show_time()
+    histograms()
+    plot3d()
